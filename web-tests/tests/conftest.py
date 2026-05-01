4c31141ad87be8f1ebd42637cc8d519e3265362c
Mateus Farias <mateus.fsantos08@gmail.com>
2026-05-01 10:33:30 -0300

    fix: corrige compatibilidade do ChromeDriver no Windows 64bits

--- web-tests/tests/conftest.py
+++ web-tests/tests/conftest.py
@@ -2,9 +2,7 @@ import pytest
 import os
 from dotenv import load_dotenv
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
 
 load_dotenv()
 
@@ -12,12 +10,12 @@ load_dotenv()
 @pytest.fixture
 def driver():
     options = Options()
-    options.add_argument("--headless")
+
+    options.add_argument("--headless=new")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
 
-    service = Service(ChromeDriverManager().install())
-    navegador = webdriver.Chrome(service=service, options=options)
+    navegador = webdriver.Chrome(options=options)
     yield navegador
     navegador.quit()
 
