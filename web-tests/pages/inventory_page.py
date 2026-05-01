9ce1ff6049533d4cd3bb54ffab26bd1817c8f731
Mateus Farias <mateus.fsantos08@gmail.com>
2026-05-01 11:30:33 -0300

    fix: corrige localizador do botão adicionar ao carrinho

--- web-tests/pages/inventory_page.py
+++ web-tests/pages/inventory_page.py
@@ -1,16 +1,20 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 from pages.base_page import BasePage
 
 class InventoryPage(BasePage):
     TITULO = (By.CLASS_NAME, "title")
-    BTN_ADD_PRODUTO = (By.CSS_SELECTOR, "[data-test='add-to-cart-sauce-labs-backpack']")
+    BTN_ADD_PRODUTO = (By.ID, "add-to-cart-sauce-labs-backpack")
     ICONE_CARRINHO = (By.CLASS_NAME, "shopping_cart_link")
+    BADGE_CARRINHO = (By.CLASS_NAME, "shopping_cart_badge")
 
     def obter_titulo(self):
         return self.find(self.TITULO).text
 
     def adicionar_produto_ao_carrinho(self):
         self.click(self.BTN_ADD_PRODUTO)
+        # Espera o badge do carrinho aparecer confirmando que o produto foi adicionado
+        self.wait.until(EC.presence_of_element_located(self.BADGE_CARRINHO))
 
     def ir_para_carrinho(self):
         self.click(self.ICONE_CARRINHO)
