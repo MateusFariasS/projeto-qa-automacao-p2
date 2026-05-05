03b7e9e6593978b6d4333a1cb05524eae2fe263b
Mateus Farias <mateus.fsantos08@gmail.com>
2026-05-05 15:00:38 -0300

    fix:alterações na base e no conftest

--- web-tests/pages/base_page.py
+++ web-tests/pages/base_page.py
@@ -1,6 +1,5 @@
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.action_chains import ActionChains
 
 TIMEOUT = 20
 NAV_TIMEOUT = 5
@@ -15,8 +14,8 @@ class BasePage:
         return self.wait.until(EC.presence_of_element_located(locator))
 
     def click(self, locator):
-        btn = self.wait.until(EC.element_to_be_clickable(locator))
-        ActionChains(self.driver).move_to_element(btn).click().perform()
+        element = self.wait.until(EC.element_to_be_clickable(locator))
+        self.driver.execute_script("arguments[0].click();", element)
 
     def type(self, locator, text):
         field = self.wait.until(EC.visibility_of_element_located(locator))
